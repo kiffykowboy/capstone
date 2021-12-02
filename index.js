5c6ec1a137af030c9b57211fc921b87c6a2c28a3
kiffykowboy <cpgreene11@gmail.com>
2021-12-01 22:54:24 -0600

    ?

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const {list, show, create, update, remove} = require('./controllers/posts')
-const cors = require('cors');
-app.use(cors({
-    origin: '*'
-}));
-
 const PORT = process.env.PORT || 5000;
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 app.use(bodyParser.json());
 
 // list route
